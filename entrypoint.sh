a9b3d124bc48037c89dd640d4f4b15dc7834c211
vertipl <vertical0520@gmail.com>
2017-03-02 15:08:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #start bota.
 cd /opt/ts3soundboard/
-chmod 777 ts3bot
+chmod 777 sinusbot
 chmod 777 /opt/ts3soundboard/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64
-./ts3bot -update
+./sinusbot -update
 LC_ALL="en_US.UTF-8" xinit /opt/ts3soundboard/ts3bot -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac
