011901c4ea8e85d74328204ad16eee0f774edd1e
vertipl <vertical0520@gmail.com>
2017-03-02 15:23:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 cd /opt/ts3soundboard/
 chmod 777 sinusbot
 chmod 777 /opt/ts3soundboard/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64
-./sinusbot -update
-LC_ALL="en_US.UTF-8" xinit /opt/ts3soundboard/ts3bot -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac
+LC_ALL="en_US.UTF-8" xinit /opt/ts3soundboard/sinusbot -RunningAsRootIsEvilAndIKnowThat=true -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac
